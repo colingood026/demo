650bca52fa465dfbff67c6b919bceec2db27d1e1
colin.lee <colin.lee@Colin-E7470.ebizprise.corp>
2016-04-22 17:11:45 +0800

    INV_ITEM_DAO新增條件搜尋

--- qmi-ui-poc/src/main/java/model/INV_ITEM_DAO.java
+++ qmi-ui-poc/src/main/java/model/INV_ITEM_DAO.java
@@ -33,7 +33,7 @@ public class INV_ITEM_DAO {
 //			e.printStackTrace();
 //		}
 //	}
-	
+	//SELECT_ALL
 	private final String SELECT_ALL="select * from INV_ITEM";
 	public List<INV_ITEM_VO> select_all(){
 		List<INV_ITEM_VO> result=null;
@@ -44,12 +44,9 @@ public class INV_ITEM_DAO {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			conn=DriverManager.getConnection(URL, USER, PASSWORD);
 //			conn=ds.getConnection();
-			stmt=conn.prepareStatement(SELECT_ALL);
-			
-			rs=stmt.executeQuery();
-			
-			result=new ArrayList<INV_ITEM_VO>();
-			
+			stmt=conn.prepareStatement(SELECT_ALL);			
+			rs=stmt.executeQuery();			
+			result=new ArrayList<INV_ITEM_VO>();			
 			while(rs.next()){
 				INV_ITEM_VO bean=new INV_ITEM_VO();
 				bean.setCNT_NO(rs.getString("CNT_NO"));
@@ -70,20 +67,133 @@ public class INV_ITEM_DAO {
 		} catch (ClassNotFoundException e) {			
 			e.printStackTrace();
 		} finally{						
-			jdbcClose.allClose(conn, stmt, rs);
-			
+			jdbcClose.allClose(conn, stmt, rs);			
 		}		
 		return result;
 	}
-	
-	
-	
-	
+	//--------------用色號搜巡SELECT_BY_COL_NO
+	private static final String SELECT_BY_COL_NO="select * from INV_ITEM where COL_NO=?";
+	public List<INV_ITEM_VO> select_by_colNo(String colNo){
+		List<INV_ITEM_VO> result=null;
+		Connection conn=null;
+		PreparedStatement stmt=null;
+		ResultSet rs=null;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn=DriverManager.getConnection(URL, USER, PASSWORD);
+//			conn=ds.getConnection();
+			stmt=conn.prepareStatement(SELECT_BY_COL_NO);
+			stmt.setString(1, colNo);
+			rs=stmt.executeQuery();			
+			result=new ArrayList<INV_ITEM_VO>();			
+			while(rs.next()){
+				INV_ITEM_VO bean=new INV_ITEM_VO();
+				bean.setCNT_NO(rs.getString("CNT_NO"));
+				bean.setCOL_NO(rs.getString("COL_NO"));
+				bean.setLOC_CODE(rs.getString("LOC_CODE"));
+				bean.setLOT_ID(rs.getString("LOT_ID"));
+				bean.setMAT_01(rs.getString("MAT_01"));
+				bean.setPUR_NO(rs.getString("PUR_NO"));
+				bean.setREC_DATE(rs.getDate("REC_DATE"));
+				bean.setSTOCK_QTY(rs.getDouble("STOCK_QTY"));
+				bean.setUNT_RQ(rs.getString("UNT_RQ"));
+				bean.setWID_TH(rs.getDouble("WID_TH"));
+				result.add(bean);
+			}
+			
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {			
+			e.printStackTrace();
+		} finally{						
+			jdbcClose.allClose(conn, stmt, rs);			
+		}		
+		return result;
+	}	
+	//--------------------------用料號搜尋SELECT_BY_MAT_01
+	private static final String SELECT_BY_MAT_01="select * from INV_ITEM where MAT_01=?";
+	public List<INV_ITEM_VO> select_by_mat01(String mat01){
+		List<INV_ITEM_VO> result=null;
+		Connection conn=null;
+		PreparedStatement stmt=null;
+		ResultSet rs=null;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn=DriverManager.getConnection(URL, USER, PASSWORD);
+//			conn=ds.getConnection();
+			stmt=conn.prepareStatement(SELECT_BY_MAT_01);
+			stmt.setString(1, mat01);
+			rs=stmt.executeQuery();			
+			result=new ArrayList<INV_ITEM_VO>();			
+			while(rs.next()){
+				INV_ITEM_VO bean=new INV_ITEM_VO();
+				bean.setCNT_NO(rs.getString("CNT_NO"));
+				bean.setCOL_NO(rs.getString("COL_NO"));
+				bean.setLOC_CODE(rs.getString("LOC_CODE"));
+				bean.setLOT_ID(rs.getString("LOT_ID"));
+				bean.setMAT_01(rs.getString("MAT_01"));
+				bean.setPUR_NO(rs.getString("PUR_NO"));
+				bean.setREC_DATE(rs.getDate("REC_DATE"));
+				bean.setSTOCK_QTY(rs.getDouble("STOCK_QTY"));
+				bean.setUNT_RQ(rs.getString("UNT_RQ"));
+				bean.setWID_TH(rs.getDouble("WID_TH"));
+				result.add(bean);
+			}
+			
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {			
+			e.printStackTrace();
+		} finally{						
+			jdbcClose.allClose(conn, stmt, rs);			
+		}		
+		return result;
+	}		
+	//--------------------------料號+色號搜尋SELECT_BY_COL_NO_AND_MAT_01
+	private static final String SELECT_BY_COL_NO_AND_MAT_01="select * from INV_ITEM where COL_NO=? and MAT_01=?";
+	public List<INV_ITEM_VO> select_by_colNoAndmat01(String colNo,String mat01){
+		List<INV_ITEM_VO> result=null;
+		Connection conn=null;
+		PreparedStatement stmt=null;
+		ResultSet rs=null;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn=DriverManager.getConnection(URL, USER, PASSWORD);
+//			conn=ds.getConnection();
+			stmt=conn.prepareStatement(SELECT_BY_COL_NO_AND_MAT_01);
+			stmt.setString(1, colNo);
+			stmt.setString(2, mat01);
+			rs=stmt.executeQuery();			
+			result=new ArrayList<INV_ITEM_VO>();			
+			while(rs.next()){
+				INV_ITEM_VO bean=new INV_ITEM_VO();
+				bean.setCNT_NO(rs.getString("CNT_NO"));
+				bean.setCOL_NO(rs.getString("COL_NO"));
+				bean.setLOC_CODE(rs.getString("LOC_CODE"));
+				bean.setLOT_ID(rs.getString("LOT_ID"));
+				bean.setMAT_01(rs.getString("MAT_01"));
+				bean.setPUR_NO(rs.getString("PUR_NO"));
+				bean.setREC_DATE(rs.getDate("REC_DATE"));
+				bean.setSTOCK_QTY(rs.getDouble("STOCK_QTY"));
+				bean.setUNT_RQ(rs.getString("UNT_RQ"));
+				bean.setWID_TH(rs.getDouble("WID_TH"));
+				result.add(bean);
+			}
+			
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {			
+			e.printStackTrace();
+		} finally{						
+			jdbcClose.allClose(conn, stmt, rs);			
+		}		
+		return result;
+	}	
 	//-------------------test------------------------
 	public static void main(String args[]){
 		INV_ITEM_DAO dao=new INV_ITEM_DAO();
 		
-		List<INV_ITEM_VO> allEquip=dao.select_all();
+		List<INV_ITEM_VO> allEquip=dao.select_by_colNoAndmat01("001-BT","S-2727");
 		
 //		JSONArray json=new JSONArray(allEquip);
 		System.out.println("allEquip="+allEquip);
